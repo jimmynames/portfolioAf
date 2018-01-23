873adafc5783e7349a6aac821305e26aee1d2984
jimmynames <jim.me.ryan@gmail.com>
2018-01-23 23:13:02 +0000

    updated path to images

--- src/pages/me.js
+++ src/pages/me.js
@@ -1,8 +1,8 @@
 import React from "react"
 import styled from 'styled-components';
-import me from './images/me.png'
-import plant from './images/plantlife.png'
-import fish from './images/fish.png'
+import me from './../images/me.png'
+import plant from './../images/plantlife.png'
+import fish from './../images/fish.png'
 
 const Page = styled.div`
   width: 100%;
