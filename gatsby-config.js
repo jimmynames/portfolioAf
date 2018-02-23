00e59b055fe0f7bdcb79c57984784ecb9bd24bf0
Jimmy Names <jim.me.ryan@gmail.com>
2018-02-23 14:38:44 +0000

    added google font

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,22 @@
 module.exports = {
   siteMetadata: {
-    title: `Jimmy Names Codes`,
+    title: `Jimmy Names Codes`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     {
-    resolve: `gatsby-plugin-google-analytics`,
-    options: {
-      trackingId: "UA-112975901-1",
-      anonymize: true,
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-112975901-1',
+        anonymize: true
+      }
     },
-  }
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [ `Lato` ]
+      }
+    }
   ]
 }
